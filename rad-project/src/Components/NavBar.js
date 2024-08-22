f2c0ff09c770a2b09ee2b8533bd5a62a13ea1c3e
Thisuli Liyanaarachchi <140165598+Thisu2002@users.noreply.github.com>
2024-08-22 14:37:25 +0530

    navbarjs

---
+++ rad-project/src/Components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import logo from "../images/Logo.png";
+import user from "../images/user.png";
+import "../styles/NavBar.css";
+import { HashLink as Link } from "react-router-hash-link";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <Link to="/#home">
+        <img src={logo} alt="Logo" className="logo"/>
+      </Link>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/#home">Home</Link>
+        </li>
+        <li>
+          <Link to="/#about-us">About Us</Link>
+        </li>
+        <li>
+          <Link to="/#services">Services</Link>
+        </li>
+        <li>
+          <Link to="/#contact-us">Contact Us</Link>
+        </li>
+        <li>
+          <Link to="/user">
+            <div className="login-link">
+              Login
+              <img
+                src={user}
+                alt="user"
+                className="user-icon"
+              />
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
+
