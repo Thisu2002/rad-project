046b8a2a7176e6280bad1698231c9477f3645625
Raveesha23 <raveeshagihani@gmail.com>
2024-09-07 03:35:35 +0530

    change

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,49 +3,42 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const vetController = require('../controllers/vetController');
 const ownerController = require('../controllers/ownerController');
-const appointmentController = require('../controllers/appointmentController');
 const petController = require('../controllers/petController');
 
-// Signup route for general users (including admins)
+// Signup route
 router.post('/signup', authController.signup);
 
-// Login route for authentication
+// Login route
 router.post('/login', authController.login);
 
-// Vet signup route
+// VetSignup route
 router.post('/vetSignup', vetController.vetSignup);
 
-// Get all pet owners
+// PetOwner route
 router.get('/pet-owners', ownerController.getAllPetOwners);
 
-// Get a single pet owner by ID
+// View PetOwner
 router.get('/pet-owners/view-owner/:id', ownerController.getPetOwnerById);
 
-// Delete a pet owner by ID
+// Delete pet owner
 router.delete('/pet-owners/:id', ownerController.deletePetOwnerById);
 
-// Get all vets
+// Vet route
 router.get('/vets', vetController.getAllVets);
 
-// Get a single vet by ID
 router.get('/vets/view-vet/:id', vetController.getVetById);
 
-// Delete a vet by ID
+// Delete vet
 router.delete('/vets/:id', vetController.deleteVetById);
 
-// Edit vet details by ID
+//Edit Vet by Id
 router.put('/vets/edit-vet/:id', vetController.editVetById);
 
-// Get appointments count
-router.get('/appointments/count', appointmentController.getAppointmentsCount);
-
-// Get pets count
-router.get('/pets/count', petController.getPetsCount);
-
-// Get pet owners count
-router.get('/pet-owners/count', ownerController.getOwnersCount);
+// Fetch pets by ownerID
+router.get('/pets/:ownerID', petController.getPetsByOwner);
 
 //add pet
-router.post('/pets/add-pet', petController.addPet);
+router.post('/add-pet', petController.addPet);
+
 
 module.exports = router;
